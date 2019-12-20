189877d027595e0a1d7b30bb7b0fb0bbf4f0ab6c
meonst <smh2153@gmail.com>
2019-12-20 22:31:03 +0900

    trying to show all the skills a hero starts with + alternative skills such as (Greymane worgen form), (Alexstraszaa dragon form)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,9 +8,9 @@ import {
     Link,
     useParams,
   } from "react-router-dom";
-  
 
 var lang = 'enus'
+var cooldown, energycost, lifecost, necessityStyle, specialMount, singleHeroic, hasActivable, isViking, hasAlternative, qAlt, wAlt, eAlt
 //MainPage is the default page
 class MainPage extends React.Component {
     
@@ -28,6 +28,7 @@ class MainPage extends React.Component {
 
 
     render() {
+        
         return this.state
     }
 }
@@ -90,25 +91,133 @@ class HeroPage extends React.Component {
     async componentDidMount() {
         var herodat;
         lang = this.props.language
-        console.log(this.props.language)
         var fetchurl ='http://localhost:8000/heroesapi/' + this.props.link + '?lang=' + lang;
         await fetch(fetchurl).then(value => value.json()).then(value => {herodat = value['herodata']})
+        console.log(herodat)
+        if (herodat['unitId'] === 'HeroDeathwing' || herodat['unitId'] === 'HeroTracer') {singleHeroic = true} else {singleHeroic = false}
+        if (herodat['abilities']['activable'] === undefined) {hasActivable = false} else {hasActivable = true}
+        if (herodat['abilities']['mount'][0]['nameId'] !== 'Mount') {specialMount = true} else {specialMount = false}
+        if (herodat['unitId'] === 'HeroLostVikingsController') {isViking = true} else {isViking = false}
+        qAlt = 'empty'; wAlt = 'empty'; eAlt = 'empty'
+        
+
+        //Alternative Skills
+        hasAlternative = false
+
+        //Abathur
+        if (herodat['unitId'] === 'HeroAbathur') {
+            hasAlternative = true
+            qAlt = herodat['heroUnits'][0]['AbathurSymbiote']['abilities']['basic'][0]
+            wAlt = herodat['heroUnits'][0]['AbathurSymbiote']['abilities']['basic'][1]
+            eAlt = herodat['heroUnits'][0]['AbathurSymbiote']['abilities']['basic'][2]
+        }
+        //Alexstrasza
+        if (herodat['unitId'] === 'HeroAlexstrasza') {
+            hasAlternative = true
+            qAlt = herodat['heroUnits'][0]['HeroAlexstraszaDragon']['abilities']['basic'][0]
+            wAlt = herodat['heroUnits'][0]['HeroAlexstraszaDragon']['abilities']['basic'][1]
+            eAlt = herodat['heroUnits'][0]['HeroAlexstraszaDragon']['abilities']['basic'][2]
+        }
+        //Chen
+        if (herodat['unitId'] === 'HeroChen') {
+            hasAlternative = true
+            qAlt = herodat['subAbilities'][0]['ChenStormEarthFire|ChenStormEarthFire|Heroic']['basic'][0]
+            wAlt = herodat['subAbilities'][0]['ChenStormEarthFire|ChenStormEarthFire|Heroic']['basic'][1]
+            eAlt = herodat['subAbilities'][0]['ChenStormEarthFire|ChenStormEarthFire|Heroic']['basic'][2]
+        }
+        //Deathwing
+        if (herodat['unitId'] === 'HeroDeathwing') {
+            hasAlternative = true
+            wAlt = herodat['subAbilities'][0]['DeathwingFormSwitch|DeathwingFormSwitch|Active']['basic'][0]
+            eAlt = herodat['subAbilities'][0]['DeathwingFormSwitch|DeathwingFormSwitch|Active']['basic'][1]
+        }
+        //D.Va
+        if (herodat['unitId'] === 'HeroDVaMech') {
+            hasAlternative = true
+            eAlt = herodat['heroUnits'][0]['HeroDVaPilot']['abilities']['basic'][2]
+        }
+        //Greymane
+        if (herodat['unitId'] === 'HeroGreymane') {
+            hasAlternative = true
+            qAlt = herodat['subAbilities'][0]['GreymaneWorgenForm|GreymaneWorgenForm|Trait|True']['basic'][0]
+            eAlt = herodat['subAbilities'][0]['GreymaneWorgenForm|GreymaneWorgenForm|Trait|True']['basic'][1]
+        }
+        //Leoric
+        if (herodat['unitId'] === 'HeroLeoric') {
+            hasAlternative = true
+            qAlt = herodat['subAbilities'][0]['LeoricUndyingTrait|LeoricUndyingTrait|Trait|True']['basic'][0]
+            wAlt = herodat['subAbilities'][0]['LeoricUndyingTrait|LeoricUndyingTrait|Trait|True']['basic'][1]
+        }
+        //Ragnaros
+        if (herodat['unitId'] === 'HeroRagnaros') {
+            hasAlternative = true
+            qAlt = herodat['heroUnits'][0]['RagnarosBigRag']['abilities']['basic'][0]
+            wAlt = herodat['heroUnits'][0]['RagnarosBigRag']['abilities']['basic'][1]
+            eAlt = herodat['heroUnits'][0]['RagnarosBigRag']['abilities']['basic'][2]
+        }
+        //Uther
+        if (herodat['unitId'] === 'HeroUther') {
+            hasAlternative = true
+            qAlt = herodat['subAbilities'][0]['UtherEternalDevotion|UtherEternalDevotion|Trait']['basic'][0]
+        }
         var Abilities = 
+            <div>
+                <div id='skills'>             
+                    <HeroSkill
+                        value={herodat['abilities']['trait'][0]}
+                    />
 
-            <div id='skills'>                
-                <HeroSkill
-                    value={herodat['abilities']['trait'][0]}
-                />
-                <HeroSkill
-                    value={herodat['abilities']['basic'][0]}
-                />
-                <HeroSkill
-                    value={herodat['abilities']['basic'][1]}
-                />
-                <HeroSkill
-                    value={herodat['abilities']['basic'][2]}
-                />
+                    <HeroSkill
+                        value={herodat['abilities']['basic'][0]}
+                    />
 
+                    <HeroSkill
+                        value={herodat['abilities']['basic'][1]}
+                    />
+
+                    <HeroSkill
+                        value={herodat['abilities']['basic'][2]}
+                    />
+
+                    <HeroSkill
+                        value={herodat['abilities']['heroic'][0]}
+                        style={{'display': singleHeroic ? 'block' : 'none'}}
+                    />
+                    <HeroSkill
+                        value={hasActivable ? herodat['abilities']['activable'][0] : undefined}
+                        style={{'display': hasActivable ? 'block' : 'none'}}
+                    />
+                    <HeroSkill
+                        value={isViking ? herodat['abilities']['activable'][1] : undefined}
+                        style={{'display': isViking ? 'block' : 'none'}}
+                    />
+                    <HeroSkill
+                        value={isViking ? herodat['abilities']['activable'][2] : undefined}
+                        style={{'display': isViking ? 'block' : 'none'}}
+                    />
+                    <HeroSkill
+                        value={isViking ? herodat['abilities']['activable'][3] : undefined}
+                        style={{'display': isViking ? 'block' : 'none'}}
+                    />
+                    <Mount
+                        value={herodat['abilities']['mount'][0]}
+                        style={{'display': specialMount ? 'block' : 'none'}}
+                    />
+                </div>
+                <div id='alternativeSkills'>
+                    <HeroSkill
+                        value='empty'
+                    />
+                    <HeroSkill
+                        value={qAlt}
+                    />
+                    <HeroSkill
+                        value={wAlt}
+                    />
+                    <HeroSkill
+                        value={eAlt}
+                    />
+                </div>
             </div>
 
         var Talents = 
@@ -157,16 +266,11 @@ class HeroPage extends React.Component {
         
             
         this.setState(All)
-        //this.setState(Talents)
-
-        
-
     }
     
     render() {
         return (
             <div>
-
                 <div>
                     {this.state}
                 </div>
@@ -174,13 +278,17 @@ class HeroPage extends React.Component {
         )
     }
 }
-//HeroSkill will render the skills of a certain Hero
-class HeroSkill extends React.Component{
+class Mount extends React.Component{
     constructor(props) {
         super(props)
         this.state = {hover: false}
     }
     render() {
+        if (this.props.value !== undefined) {
+            if (this.props.value.hasOwnProperty('cooldownTooltip')) {
+                cooldown = this.props.value['cooldownTooltip']
+            } else {cooldown = undefined}
+        }
         const Tooltip = {
             'display': this.state.hover ? 'block' : 'none',
             'position': 'absolute',
@@ -191,14 +299,82 @@ class HeroSkill extends React.Component{
             'padding': '3px',
             'zIndex': '99999',
         }
+        return(
+            <div 
+                id="mount"
+                style={this.props.style}
+                onMouseOver={() => this.setState({hover: true})}
+                onMouseOut={() => this.setState({hover: false})}
+            >
+                <SkillImage
+                    value={this.props.value['icon']}
+                    id={this.props.value['nameId']}
+                    name={this.props.value['name']}
+                    description={this.props.value['fullTooltip']}
+                />
+                <span
+                    id="tooltip"
+                    style={Tooltip}
+                >
+                    <Description
+                        name={this.props.value['name']}
+                        energycost={energycost}
+                        lifecost={lifecost}
+                        cooldown={cooldown}
+                        value={this.props.value['fullTooltip']}
+                    />
+                
+                </span>
+            </div>
+        )
+    }
+}
+
+//HeroSkill will render the skills of a certain Hero
+class HeroSkill extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {hover: false}
+    }
+    render() {
+        if (this.props.value !== undefined) {
+            if (this.props.value.hasOwnProperty('energyTooltip')) {
+                energycost = this.props.value['energyTooltip']
+            } else {energycost = undefined}
+            if (this.props.value.hasOwnProperty('lifeTooltip')) {
+                lifecost = this.props.value['lifeTooltip']
+            } else {lifecost = undefined}
+            if (this.props.value.hasOwnProperty('cooldownTooltip')) {
+                cooldown = this.props.value['cooldownTooltip']
+            } else {cooldown = undefined}
+        }
+
+        const Tooltip = {
+            'display': this.state.hover ? 'block' : 'none',
+            'position': 'absolute',
+            'backgroundColor': 'white',
+            'width': '400px',
+            'borderStyle': 'solid',
+            'borderColor': 'grey',
+            'padding': '3px',
+            'zIndex': '99999',
+        }
+        if (this.props.value === 'empty') {
+            return(
+                <div id="emptySkill"/>
+            )
+        }
 
         if (this.props.value === undefined) {
             return(
-                <div id="skill">
+                <div 
+                    id="skill"
+                    style={this.props.style}
+                >
                     <SkillImage
                         value={'hud_btn_bg_ability_cancel.png'}
                         id="undefined"
-                        name="ubdefined"
+                        name="undefined"
                         description="Skill does not exist"
                     />
                 </div>
@@ -209,14 +385,15 @@ class HeroSkill extends React.Component{
             return(
                 <div 
                     id="skill"
+                    style={this.props.style}
                     onMouseOver={() => this.setState({hover: true})}
                     onMouseOut={() => this.setState({hover: false})}
                 >
                     <SkillImage
                         value={this.props.value['icon']}
+                        id={this.props.value['nameId']}
                         name={this.props.value['name']}
                         description={this.props.value['fullTooltip']}
-                        id={this.props.value['nameId']}
                     />
                     <span
                         id="tooltip"
@@ -224,6 +401,9 @@ class HeroSkill extends React.Component{
                     >
                         <Description
                             name={this.props.value['name']}
+                            energycost={energycost}
+                            lifecost={lifecost}
+                            cooldown={cooldown}
                             value={this.props.value['fullTooltip']}
                         />
                     
@@ -273,6 +453,16 @@ class Talent extends React.Component {
         this.state = {hover: false}
     }
     render() {
+        if (this.props.value.hasOwnProperty('energyTooltip')) {
+            energycost = this.props.value['energyTooltip']
+        } else {energycost = undefined}
+        if (this.props.value.hasOwnProperty('lifeTooltip')) {
+            lifecost = this.props.value['lifeTooltip']
+        } else {lifecost = undefined}
+        if (this.props.value.hasOwnProperty('cooldownTooltip')) {
+            cooldown = this.props.value['cooldownTooltip']
+        } else {cooldown = undefined}
+
         const Tooltip = {
             'display': this.state.hover ? 'block' : 'none',
             'position' : 'absolute',
@@ -299,6 +489,9 @@ class Talent extends React.Component {
                 >
                     <Description
                         name={this.props.value['name']}
+                        energycost={energycost}
+                        lifecost={lifecost}
+                        cooldown={cooldown}
                         value={this.props.value['fullTooltip']}
                     />
                     
@@ -311,11 +504,22 @@ class Talent extends React.Component {
 //Do you like Tooltips? Well I do!
 class Description extends React.Component {
     render() {
+        if (this.props.energycost === undefined && this.props.lifecost === undefined && this.props.cooldown === undefined) {
+            necessityStyle = {'display': 'none'}} else {necessityStyle = {'display': 'block'}}
         return(
             
             <div>
                 <div id="discriptionTitle">
                     {this.props.name}
+                </div>
+                <div id='necessity' style={necessityStyle}>
+                    <div id='costs'>
+                        {this.props.energycost}
+                        {this.props.lifecost}
+                    </div>
+                    <div id='cooldown'>
+                        {this.props.cooldown}
+                    </div>
                 </div>
                 {this.props.value}
                 
@@ -330,12 +534,13 @@ class TopBar extends React.Component {
         super(props)
         this.state = {hover: false}
     }
+
     render() {
         const LanguageBar = {
             'display': this.state.hover ? 'block' : 'none',
             'position' : 'absolute',
             'backgroundColor': 'white',
-            'width': '400px',
+            'width': 'auto',
             'borderStyle': 'solid',
             'borderColor': 'grey',
             'padding': '3px',
@@ -368,20 +573,19 @@ class TopBar extends React.Component {
                     onMouseOut={() => this.setState({hover: false})}
                 >
                     LANG
-                    <span
-                        style={LanguageBar}
-                    >
-                        <Link
-                            to={'/enus' + sliced}
-                        >
-                            English
-                        </Link>
-
-                        <Link
-                            to={'/kokr' + sliced}
-                        >
-                            한국어
-                        </Link>
+                    <span style={LanguageBar}>
+                        <Link to={'/refresh/dede' + sliced} id='Langs'>Deutsch</Link><br/>
+                        <Link to={'/refresh/enus' + sliced} id='Langs'>English</Link><br/>
+                        <Link to={'/refresh/eses' + sliced} id='Langs'>Español(EU)</Link><br/>
+                        <Link to={'/refresh/esmx' + sliced} id='Langs'>Español(AL)</Link><br/>
+                        <Link to={'/refresh/frfr' + sliced} id='Langs'>Français</Link><br/>
+                        <Link to={'/refresh/itit' + sliced} id='Langs'>Italiano</Link><br/>
+                        <Link to={'/refresh/kokr' + sliced} id='Langs'>한국어</Link><br/>
+                        <Link to={'/refresh/plpl' + sliced} id='Langs'>Polski</Link><br/>
+                        <Link to={'/refresh/ptbr' + sliced} id='Langs'>Português</Link><br/>
+                        <Link to={'/refresh/ruru' + sliced} id='Langs'>Русский</Link><br/>
+                        <Link to={'/refresh/zhcn' + sliced} id='Langs'>简体中文</Link><br/>
+                        <Link to={'/refresh/zhtw' + sliced} id='Langs'>繁體中文</Link><br/>
                     </span>
                 
                 </div>
@@ -429,11 +633,13 @@ class RealPage extends React.Component {
                 <div>
                     <TopBar/>
                     <Switch>
+                        <Route path="/refresh" children={<Refresh/>}/>
                         <Route exact path="/">
                             <MainPage/>
                         </Route>
                         <Route exact path="/:language" children={<MainPageLanguage/>}/>
                         <Route path="/:language/:herolink/:talents" children={<GetHeroLink/>}/>
+
                     
                     </Switch>
                 </div>
@@ -442,6 +648,11 @@ class RealPage extends React.Component {
     }
 }
 
+function Refresh() {
+    var new_path = window.location.pathname.slice(8)
+    window.location.pathname = new_path
+    return(<div/>)
+}
 
 //Rendering the RealPage
 ReactDOM.render(
