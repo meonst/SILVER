678bec81551d7c8ec6f0a1a133cee14d306334b2
Minhyeok Seo <smh2153@gmail.com>
2020-08-20 16:44:35 +0900

    added patch data 91376

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 
 //var lang = (navigator.language || navigator.userLanguage).toLowerCase().replace('-', '');
 var lang = 'enus'
-const version = '80702'
+const version = '81376'
 var cooldown, energycost, lifecost, necessityStyle, sharedTalent
 const talentShareColor = []
 talentShareColor[0] = {borderColor: '#cd7f32'}
