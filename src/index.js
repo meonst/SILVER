eb741d9d5f259cb473a1bc7f47a2c71d83efc10f
Minhyeok Seo <smh2153@gmail.com>
2021-03-05 17:52:39 +0900

    hotfix 84249

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 
 //var lang = (navigator.language || navigator.userLanguage).toLowerCase().replace('-', '');
 var lang = 'enus'
-const version = '84200'
+const version = '84249'
 var cooldown, energycost, lifecost, necessityStyle, sharedTalent
 const talentShareColor = []
 talentShareColor[0] = {borderColor: '#cd7f32'}
